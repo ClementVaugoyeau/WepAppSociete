f48d9d5b750376d69d84371648213ad5da9f2e9a
vyou <van-rottana.you@hotmail.com>
2022-02-02 11:22:36 +0100

    first commit back

---
+++ BackHorodatage/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BackHorodatage;
+using BackHorodatage.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Messagerie.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly PostgreSqlContext _context;
+
+        public UsersController(PostgreSqlContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.users.ToListAsync();
+        }
+
+
+
+    }
+}
---
+++ BackHorodatage/PostgreSqlContext.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+//using System.Linq;
+//using System.Web;
+//using Messagerie;
+//using Npgsql;
+using BackHorodatage.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackHorodatage
+{
+    public class PostgreSqlContext : DbContext
+    {
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseNpgsql(@"Server=localhost;Port=5432;Database=Horodatage;User Id=postgres;Password=root");
+        //}
+        public PostgreSqlContext(DbContextOptions<PostgreSqlContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
