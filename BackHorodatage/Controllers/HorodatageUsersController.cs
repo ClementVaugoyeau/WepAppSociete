fbb6578d6d8b4843538fddbf934ebec5ad6e7e08
vyou <van-rottana.you@hotmail.com>
2022-02-03 14:32:12 +0100

    commit ajout api crud users

--- BackHorodatage/Controllers/HorodatageUsersController.cs
+++ BackHorodatage/Controllers/HorodatageUsersController.cs
@@ -30,8 +30,6 @@ namespace BackHorodatage.Controllers
         }
 
         // POST: HorodatageUsers/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         public async Task<ActionResult<HorodatageUser>> PostHorodatageUser(HorodatageUser horodatageUser)
         {
--- BackHorodatage/Controllers/UsersController.cs
+++ BackHorodatage/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using BackHorodatage;
 using BackHorodatage.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace Messagerie.Controllers
 {
     [Route("[controller]")]
@@ -23,7 +24,7 @@ namespace Messagerie.Controllers
             return await _context.users.ToListAsync();
         }
 
-        [HttpGet("{prenom}")]
+        [HttpGet("/prenom/{prenom}")]
         public async Task<User> GetUserByPrenom(string prenom)
         {
             var query = from st in _context.users
@@ -35,12 +36,76 @@ namespace Messagerie.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<User>> PostOrderMaster(User user)
+        public async Task<ActionResult<User>> PostUser(User user)
         {
             _context.users.Add(user);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetUsers", new { id = user.IdUser }, user);
+        }
+
+        // GET: api/OrderMasters/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/OrderMasters/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.IdUser)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(long id)
+        {
+            return _context.users.Any(e => e.IdUser == id);
         }
 
 
