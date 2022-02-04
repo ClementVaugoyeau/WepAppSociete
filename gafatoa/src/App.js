11cd0165058a6b63a40fb3f9127b6a94db9d92d2
vyou <van-rottana.you@hotmail.com>
2022-02-04 16:30:19 +0100

    ajout read horodatage

--- gafatoa/src/App.js
+++ gafatoa/src/App.js
@@ -6,6 +6,7 @@ import Read from './Read'
 import Update from './Update'
 import PostHorodatages from './PostHorodatage/PostHorodatages'
 import LoginPage  from './LoginPage/LoginPage'
+import ReadHoro  from './ReadHoro/ReadHoro'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 var user = null ;
@@ -43,6 +44,11 @@ function App() {
                       Pointage
                 </Link>
               </button>
+              <button className='btn btn-nav'>
+                <Link to="/horodatage" className="btn-nav">
+                      Horodatage
+                </Link>
+              </button>
               <button className='btn btn-primary' onClick={logOut}>
                 <Link to="/" className="btn">Logout</Link>
               </button>
@@ -52,6 +58,7 @@ function App() {
             <Routes>
               <Route exact path='/read' element={<Read />} />
               <Route exact path='/update' element={<Update/>} />
+              <Route exact path='/horodatage' element={<ReadHoro/>} />
               <Route exact path='/create' element={<PostHorodatages/>} />
               <Route exact path='/' element={<LoginPage/>} />
             </Routes>
